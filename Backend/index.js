16646c781f68128e11721de87a17810e7a9132b4
Adrián Rivarola <adr2358@gmail.com>
2020-04-26 19:30:35 -0400

    separar typeDefs y resolvers en diferentes archivos

--- Backend/index.js
+++ Backend/index.js
@@ -2,18 +2,9 @@ require('dotenv').config();
 
 const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
-const gql = require('graphql-tag');
 
-const typeDefs = gql`
-	type Query {
-		helloWorld: String!
-	}`;
-
-const resolvers = {
-	Query: {
-		helloWorld: () => 'Hello World'
-	}
-}
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
 
 const server = new ApolloServer({
 	typeDefs,
