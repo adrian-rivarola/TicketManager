a6a1126109687eb2e2cbe7ebc34a8be193460195
Adrián Rivarola <adr2358@gmail.com>
2020-04-27 18:34:11 -0400

    permitir a usuarios listar los tickets válidos que poseen

--- Backend/graphql/resolvers/tickets.js
+++ Backend/graphql/resolvers/tickets.js
@@ -4,6 +4,20 @@ const User = require('../../models/User');
 
 const validar_usuario = require('../../utils/validar_usuario');
 
+async function ver_tickets(_, __, context) {
+	const { id } = validar_usuario(context);
+
+	const user = await User.findById(id).populate({
+		path: 'tickets',
+		populate: { path: 'event' }
+	});
+	if (!user) {
+		throw new Error('Usuario no encontrado');
+	}
+	
+	return user.tickets;
+}
+
 async function crear_ticket(_, { ticketInput }, context) {
 	const { id } = validar_usuario(context);
 	
@@ -22,11 +36,17 @@ async function crear_ticket(_, { ticketInput }, context) {
 
 	const ticket = new Ticket({ event, owner });
 
+	owner.tickets.unshift(ticket._id);
+	await owner.save();
+
 	await ticket.save();
 	return ticket
 }
 
 module.exports = {
+	Query: {
+		ver_tickets
+	},
 	Mutation: {
 		crear_ticket
 	}
