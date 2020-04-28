45dfe72b7ad3dd4784f37d5c1645d2085951371b
Adrián Rivarola <adr2358@gmail.com>
2020-04-28 09:32:19 -0400

    páginas de registro y login

--- react-app/src/pages/Login.js
+++ react-app/src/pages/Login.js
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Button } from 'semantic-ui-react';
+import { useMutation } from '@apollo/react-hooks';
+import { useForm } from '../util/hooks';
 
-export default function Login(props) {
+import gql from 'graphql-tag';
+
+export default function Register(props) {
+
+	const [errors, setErrors] = useState([]);
+	const { values, onSubmit, onChange } = useForm(loginCB, {
+		username: '',
+		password: '',
+		confirmPassword: ''
+	});
+
+	const [addUser, { loading }] = useMutation(LOGIN_MUTATION, {
+		update(_, result) {
+			console.log(result);
+			props.history.push('/');
+		},
+		onError(err) {
+			console.error(err);
+		},
+		variables: values
+	})
+
+	function loginCB() {
+		addUser();
+	}
+
 	return (
 		<div>
-			<h1>Login</h1>
+			<Form onSubmit={onSubmit} noValidate className={loading ? 'loading':''} >
+				<h1>Acceder</h1>
+				<br />
+				<Form.Input
+					type="text"
+					label="Nombre de usuario:"
+					name="username"
+					value={ values.username }
+					onChange={onChange}
+				/>
+				<Form.Input
+					label="Contraseña:"
+					type="password"
+					name="password"
+					value={ values.password }
+					onChange={onChange}
+				/>
+				<Button type="submit" color="teal" >
+					Log in
+				</Button>
+			</Form>
 		</div>
 	);
 }
+
+const LOGIN_MUTATION = gql`
+	mutation login(
+		$username: String!
+		$password: String!
+	) {
+		login(
+			username: $username
+			password: $password
+		) {
+			id username authToken
+		}
+	}
+`;
--- react-app/src/pages/Register.js
+++ react-app/src/pages/Register.js
@@ -1,10 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Button } from 'semantic-ui-react';
+import { useMutation } from '@apollo/react-hooks';
+import { useForm } from '../util/hooks';
+
+import gql from 'graphql-tag';
 
 export default function Register(props) {
 
+	const [errors, setErrors] = useState([]);
+	const { values, onSubmit, onChange } = useForm(registerCB, {
+		username: '',
+		password: '',
+		confirmPassword: ''
+	});
+
+	const [addUser, { loading }] = useMutation(REGISTER_MUTATION, {
+		update(_, result) {
+			console.log(result);
+			props.history.push('/');
+		},
+		onError(err) {
+			console.error(err);
+		},
+		variables: values
+	})
+
+	function registerCB() {
+		addUser();
+	}
+
 	return (
 		<div>
-      <h1>Registrarse</h1>
-    </div>
+			<Form onSubmit={onSubmit} noValidate className={loading ? 'loading':''} >
+				<h1>Registrarse</h1>
+				<br />
+				<Form.Input
+					type="text"
+					label="Nombre de usuario:"
+					name="username"
+					value={ values.username }
+					onChange={onChange}
+				/>
+				<Form.Input
+					label="Contraseña:"
+					type="password"
+					name="password"
+					value={ values.password }
+					onChange={onChange}
+				/>
+				<Form.Input
+					label="Confirmar contraseña:"
+					type="password"
+					name="confirmPassword"
+					value={ values.confirmPassword }
+					onChange={onChange}
+				/>
+				<Button type="submit" color="teal" >
+					Registrarse
+				</Button>
+			</Form>
+		</div>
 	);
 }
+
+const REGISTER_MUTATION = gql`
+	mutation register(
+		$password: String!
+		$username: String!
+		$confirmPassword: String!
+	) {
+		register(
+			registerInput: {
+				username: $username
+				password: $password
+				confirmPassword: $confirmPassword
+			}
+		) {
+			id username authToken
+		}
+	}
+`;
