8ff0b356e8bd8de8e0c9e13f748a2417281d63d1
Adrián Rivarola <adr2358@gmail.com>
2020-04-28 01:08:48 -0400

    agregar páginas para registro y login

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
+
+import { Container } from 'semantic-ui-react';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 export default function App() {
 	return (
-		<div>
-			<h1>Hello World</h1>
-		</div>
+		<Router>
+    	<Container>
+	    	<Route exact path='/' component={Home} />
+	    	<Route exact path='/login' component={Login} />
+	    	<Route exact path='/register' component={Register} />
+	    </Container>
+    </Router>
 	);
 }
