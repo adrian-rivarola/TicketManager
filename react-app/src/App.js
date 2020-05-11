5d8aa3d681c554b4885d6fb7e674bbbcefa904b5
Adrián Rivarola <adr2358@gmail.com>
2020-05-11 14:13:22 -0400

    separar código generado según rutas existentes con React.lazy y Suspense

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Route
@@ -10,29 +10,48 @@ import './App.css';
 import { AuthProvider } from './context/auth';
 import AuthRoute from './util/AuthRoute';
 
-import { Container } from 'semantic-ui-react';
 import MenuBar from './components/MenuBar';
+
 import Home from './pages/Home';
+
 import Login from './pages/Login';
 import Register from './pages/Register';
-import ListaEventos from './pages/ListaEventos';
-import NuevoEvento from './pages/NuevoEvento';
-import VerificarTicket from './pages/VerificarTicket';
+
+const NuevoEvento = React.lazy(() => import('./pages/NuevoEvento')),
+      ListaEventos = React.lazy(() => import('./pages/ListaEventos')),
+      VerificarTicket = React.lazy(() => import('./pages/VerificarTicket'))
 
 export default function App() {
-	return (
-		<AuthProvider>
-			<Router>
-	    	<Container>
-	    		<Route path='/' component={MenuBar} />
-		    	<Route exact path='/' component={Home} />
-		    	<Route exact path='/mis-eventos' component={ListaEventos} />
-		    	<Route exact path='/mis-eventos/nuevo' component={NuevoEvento} />
-		    	<Route exact path='/verificar-ticket' component={VerificarTicket} />
-		    	<AuthRoute exact path='/login' component={Login} />
-		    	<AuthRoute exact path='/register' component={Register} />
-		    </Container>
-	    </Router>
-	   </AuthProvider>
-	);
+  return (
+    <AuthProvider>
+      <Router>
+        <Route path="/" component={MenuBar} />
+        <Route exact path='/' component={Home} />
+
+        <Suspense fallback={ <Loader titulo="Mis Eventos" icon="group" /> }>
+          <Route exact path='/mis-eventos' component={ListaEventos} />
+          <Route exact path='/mis-eventos/nuevo' component={NuevoEvento} />
+        </Suspense>
+
+        <Suspense fallback={ <Loader titulo="VerificarTicket" icon="qrcode" /> }>
+          <Route exact path='/verificar-ticket' component={VerificarTicket} />
+        </Suspense>
+
+        <AuthRoute exact path='/login' component={Login} />
+        <AuthRoute exact path='/register' component={Register} />
+
+      </Router>
+     </AuthProvider>
+  );
 }
+
+const Loader = props => (
+  <div className="ui loading raised segment">
+    <div className="ui horizontal divider">
+      <h4 className="ui header">
+        <i aria-hidden="true" className={`${props.icon} icon`}></i>
+        {props.titulo}
+      </h4>
+    </div>
+  </div>
+);
