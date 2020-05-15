811be3a2f342fe88c0a5197aa0904eb10254eee4
Adrián Rivarola <adr2358@gmail.com>
2020-05-14 23:06:51 -0400

    crear componente para mostrar QR de un ticket

---
+++ react-app/src/components/TicketQR.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Segment } from 'semantic-ui-react';
+import QRCode from 'qrcode.react';
+
+function TicketQR({ ticket: { id, event: { name } } }) {
+	return (
+		<Segment textAlign="center" basic>
+			<QRCode
+				value={id}
+				size={256}
+        className="qr-code"
+        style={{}}
+      />
+      <hr />
+      <p><b>{name}</b></p>
+		</Segment>
+	);
+}
+
+export default React.memo(TicketQR);
