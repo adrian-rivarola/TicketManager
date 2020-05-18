78a178affe862ee12f2c8a5a4b39736ca84c90d1
Adrián Rivarola <adr2358@gmail.com>
2020-05-18 01:48:30 -0400

    combinar Header y Loader en un sólo componente

--- react-app/src/components/Header.js
+++ react-app/src/components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
-const Header = ({titulo, icono}) => (
-  <div className="ui horizontal divider">
-    <h4 className="ui header">
-      <i aria-hidden="true" className={`${icono} icon`}></i>
-      {titulo}
-    </h4>
-  </div>
-);
+const Header = ({titulo, icono, loading}) => {
+	
+	const TITUlO = (
+	  <div className="ui horizontal divider">
+	    <h4 className="ui header">
+	      <i aria-hidden="true" className={`${icono} icon`}></i>
+	      {titulo}
+	    </h4>
+	  </div>
+	);
+
+	return loading 
+		? <div className="ui loading raised segment">{ TITUlO }</div> 
+		: TITUlO
+}
 
 export default Header;
