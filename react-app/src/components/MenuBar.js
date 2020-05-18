78b80b3c5c1bf09cf48d7a2b88aae24ecd8c16f1
Adrián Rivarola <adr2358@gmail.com>
2020-05-18 00:54:51 -0400

    remover algunos elemntos del menú, usar NavLink en lugar de listener

--- react-app/src/components/MenuBar.js
+++ react-app/src/components/MenuBar.js
@@ -1,58 +1,32 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Menu } from 'semantic-ui-react';
 
 import { AuthContext } from '../context/auth';
 
 function MenuBar(props) {
-  const [activeItem, setActiveItem] = useState(props.history.location.pathname);
   const { user, logout } = useContext(AuthContext);
-  
-  const handleLogout = () => {
-    logout()
-    props.history.push("/");
-  }
-
-  useEffect(() => {
-    const unlisten = props.history.listen((location, action) => {
-      setActiveItem(location.pathname)
-    });
-
-    return () => {
-      unlisten();
-    }
-  }, [props.history]);
 
   return (
     <Menu color='teal'>
       <Menu.Item
         name={(user && user.username) || 'Inicio'}
-        active={activeItem === '/'}
-        as={Link}
+        as={NavLink}
+        exact
         to='/'
-        replace
-      />
+        replace />
       { user
         ? <React.Fragment>
             <Menu.Item
               name='MisEventos'
-              active={activeItem.startsWith('/mis-eventos')}
-              as={ Link }
-              to='/mis-eventos'
-              replace
-            />
-            <Menu.Item
-              name='VerificarTickets'
-              active={activeItem === '/verificar-ticket'}
-              as={ Link }
-              to='/verificar-ticket'
-              replace
+              as={NavLink}
+              to='/eventos'
             />
             <Menu.Menu position='right'>
               <Menu.Item
                 name='cerrar sesion'
-                onClick={handleLogout}
+                onClick={logout}
               />
             </Menu.Menu>
           </React.Fragment>
@@ -60,15 +34,13 @@ function MenuBar(props) {
         : <Menu.Menu position='right'>
             <Menu.Item
               name='Acceder'
-              active={activeItem === '/login'}
-              as={ Link }
+              as={NavLink}
               to='/login'
               replace
             />
             <Menu.Item
               name='Registrarse'
-              active={activeItem === '/register'}
-              as={ Link }
+              as={NavLink}
               to='/register'
               replace
             />
