6747b90dc9d965b13db48abaa3ef19e68e90757f
Adrián Rivarola <adr2358@gmail.com>
2020-05-25 11:09:23 -0400

    agregar IntlProvider a la App

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router,
   Route
 } from "react-router-dom";
 
+import { IntlProvider } from "react-intl";
+
 import { AuthProvider } from './context/auth';
 import { PublicRoute, PrivateRoute } from './util/AuthRoute';
+
+import messages from './messages';
 
 import MenuBar from './components/MenuBar';
 
@@ -18,18 +22,22 @@ import Register from './pages/Register';
 import './App.css';
 
 export default function App() {
+  const [locale] = useState('en');
+
   return (
     <AuthProvider>
-      <Router>
-        <Route path="/" component={MenuBar} />
-        <Route exact path='/' component={Home} />
+      <IntlProvider locale={locale} messages={messages[locale]}>
+        <Router>
+          <Route path="/" component={MenuBar} />
+          <Route exact path='/' component={Home} />
 
-        <PrivateRoute path='/eventos' component={Eventos} />
+          <PrivateRoute path='/eventos' component={Eventos} />
 
-        <PublicRoute exact path='/login' component={Login} restricted />
-        <PublicRoute exact path='/register' component={Register} restricted />
+          <PublicRoute exact path='/login' component={Login} restricted />
+          <PublicRoute exact path='/register' component={Register} restricted />
 
-      </Router>
-     </AuthProvider>
+        </Router>
+      </IntlProvider>
+    </AuthProvider>
   );
 }
