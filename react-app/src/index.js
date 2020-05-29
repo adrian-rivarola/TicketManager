0d59fd9ad8ecd6d8b1de7c11acf114f5b5630a09
Adrián Rivarola <adr2358@gmail.com>
2020-05-29 17:26:57 -0400

    agregar ServiceWorker

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,4 +2,8 @@ import ReactDOM from 'react-dom';
 
 import ApolloProvider from './ApolloProvider';
 
+import * as serviceWorker from './serviceWorker';
+
 ReactDOM.render(ApolloProvider, document.getElementById('root'));
+
+serviceWorker.register();
